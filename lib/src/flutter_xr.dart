5ae4ffa4940f87a5e34651a32ddc52209a030478
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2025-12-03 14:30:18 +0530

    save

--- lib/src/flutter_xr.dart
+++ lib/src/flutter_xr.dart
@@ -2,7 +2,6 @@
 
 import '../flutter_xr_platform_interface.dart';
 
-/// Public API exposed by `package:flutter_xr/flutter_xr.dart`.
 class FlutterXr {
   Future<String?> getPlatformVersion() {
     return FlutterXrPlatform.instance.getPlatformVersion();
